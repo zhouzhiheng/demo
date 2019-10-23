9f5aad1afd1fcef76dad7f85a5e4abc24db29335
opsigte <opsigte994@163.com>
2019-10-23 08:32:36 +0800

    9

--- src/main/java/com/opsigte/demo/jenkinsdemo/controller/DemoController.java
+++ src/main/java/com/opsigte/demo/jenkinsdemo/controller/DemoController.java
@@ -17,7 +17,7 @@ public class DemoController {
 
     @RequestMapping(value = "/test")
     public String test(String name){
-        return StringUtils.isEmpty(name) ? "test9" : name;
+        return StringUtils.isEmpty(name) ? "test10" : name;
     }
 
 }
